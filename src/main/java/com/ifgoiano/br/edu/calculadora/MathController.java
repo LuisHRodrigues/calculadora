f7bf41ab99b45edf24e6c08b1e1cd3d874366018
LuisHRodrigues <luishenriquebr505@gmail.com>
2025-03-27 17:27:00 -0300

    commit

--- src/main/java/com/ifgoiano/br/edu/calculadora/MathController.java
+++ src/main/java/com/ifgoiano/br/edu/calculadora/MathController.java
@@ -1,13 +1,9 @@
-package com.ifgoiano.br.edu.calculadora.Controller;
+package com.ifgoiano.br.edu.calculadora;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.ifgoiano.br.edu.calculadora.NumConverter;
-import com.ifgoiano.br.edu.calculadora.SimpleMath;
-import com.ifgoiano.br.edu.calculadora.UnsupportedMathOperationException;
 
 @RestController
 public class MathController {
@@ -15,24 +11,29 @@ public class MathController {
 
     // Se receber apenas um número, define que number2 = 0
     @RequestMapping(value = "/sum/{number1}/", method = RequestMethod.GET)
-    public Double sum(@PathVariable(value = "number1") String number1) throws Exception {
-        return sum(number1, "0"); // Chama o outro método passando "0" como padrão
+    public Double sum1(@PathVariable(value = "number1") String number1) throws Exception {
+        return sum(number1, "0"); // Chama o método 'sum' passando "0" como valor para number2
     }
 
     // Se receber apenas um número, define que number2 = 0
     @RequestMapping(value = "/sub/{number1}/", method = RequestMethod.GET)
-    public Double sub(@PathVariable(value = "number1") String number1) throws Exception {
-        return sub(number1, "0"); // Chama o outro método passando "0" como padrão
+    public Double sub1(@PathVariable(value = "number1") String number1) throws Exception {
+        return sub(number1, "0"); // Chama o método 'sub' passando "0" como valor para number2
     }
 
     // Se receber apenas um número, define que number2 = 1
     @RequestMapping(value = "/div/{number1}/", method = RequestMethod.GET)
-    public Double div(@PathVariable(value = "number1") String number1) throws Exception {
-        return div(number1, "1"); // Chama o outro método passando "1" como padrão
+    public Double div1(@PathVariable(value = "number1") String number1) throws Exception {
+        return div(number1, "1"); // Chama o método 'div' passando "1" como valor para number2
+    }
+
+    @RequestMapping(value = "/mean/{number1}/", method = RequestMethod.GET)
+    public Double mean1(@PathVariable(value = "number1") String number1) throws Exception {
+        return mean(number1, "0");
     }
 
     // ************** Metodo de soma **************
-    // Define um endpoint GET para somar dois números passados na URL
+    // Defindo um endpoint GET para somar dois números passados na URL
     @RequestMapping(value = "/sum/{number1}/{number2}", method = RequestMethod.GET)
     public Double sum(@PathVariable(value = "number1") String number1,
             @PathVariable(value = "number2") String number2) throws Exception {
@@ -49,7 +50,7 @@ public class MathController {
     } // ************** Fim metodo de soma **************
 
     // ************** Metodo de subtração **************
-    // Define um endpoint GET para subtrair dois numeros passados na URL
+    // Defindo um endpoint GET para subtrair dois numeros passados na URL
     @RequestMapping(value = "/sub/{number1}/{number2}", method = RequestMethod.GET)
     public Double sub(@PathVariable(value = "number1") String number1,
             @PathVariable(value = "number2") String number2) throws Exception {
@@ -66,6 +67,7 @@ public class MathController {
     }// ************** Fim metodo de subtração **************
 
     // ************** Metodo de divisão **************
+    // Definindo um endpoint GET para subtrair dois numeros passados na URL
     @RequestMapping(value = "/div/{number1}/{number2}", method = RequestMethod.GET)
     public Double div(@PathVariable(value = "number1") String number1,
             @PathVariable(value = "number2") String number2) throws Exception {
@@ -78,5 +80,20 @@ public class MathController {
                 NumConverter.convertToDouble(number2));
 
     }// ************** Fim metodo de divisão **************
+
+    // ************** Metodo de media dos numeros**************
+    @RequestMapping(value = "/mean/{number1}/{number2}", method = RequestMethod.GET)
+    public Double mean(@PathVariable(value = "number1") String number1,
+            @PathVariable(value = "number2") String number2) throws Exception {
+
+        // Verifica se os valores inseridos são numericos
+        if (!NumConverter.isNumeric(number1) & !NumConverter.isNumeric(number2)) {
+            throw new UnsupportedOperationException("Por favor, coloque um valor numerico!");
+        }
+        // Converte os valores para Double e realiza a media dos numeros
+        return math.mean(NumConverter.convertToDouble(number1),
+                NumConverter.convertToDouble(number2));
+
+    } // ************** Fim metodo de media dos numeros**************
 
 }
